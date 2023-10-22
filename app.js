e6ac9b3c9e3f43f784ebe30d13acdddb5a7168b5
ZanaMisini <118012775+ZanaMisini@users.noreply.github.com>
2023-10-22 19:55:10 +0200

    added more functions

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-var scores, currentScore, activePlayer, gamePlays;
-gamePlays = true;
+var scores, currentScore, activePlayer, gamePlays, maxScore, lastRoll, dice;
+maxScore = 0; gamePlays = true;
+
+addManualScore = () => {
+    document.querySelector('.btn--addScore').addEventListener('click', () => {
+        maxScore = document.getElementById('setMaxScore').value;
+        var text = "First to " + maxScore + ' wins!';
+        document.querySelector('.btn--field').style.display = 'none';
+        document.querySelector('.btn--addScore').style.display = 'none';
+        document.querySelectorAll('.btn--field')[1].style.display = 'block';
+        document.getElementById('paragraph').innerHTML = text;
+    })
+}
 init = () => {
+    document.querySelectorAll('.btn--field')[1].style.display = 'none';
+    addManualScore();
+    console.log(maxScore);
     gamePlays = true;
     if(gamePlays){
     scores = [0,0];
     currentScore = 0;
     activePlayer = 0;
+    lastRoll = 0;
+    dice = 0;
+    maxScore = 30;
     document.getElementById('score--0').innerHTML = 0;
     document.getElementById('score--1').innerHTML = 0;
     document.getElementById('current--0').innerHTML = 0;
@@ -25,42 +42,57 @@ init = () => {
     }
 }
 
-
 init();
 
-
 document.querySelector('.btn--roll').addEventListener('click', () => {
-    if(gamePlays){
-        var dice = Math.floor(Math.random() * 6) + 1;
-    console.log(dice);
-    document.querySelector('.dice').style.display = 'block';
-    document.querySelector('.dice').src = 'dice-' + dice + '.png';
-    if(dice !== 1){
-    currentScore += dice;
-    document.getElementById('current--' + activePlayer).textContent = currentScore;
-    }else{
-        nextPlayer();        
+    if(maxScore === 30){
+        document.querySelectorAll('.btn--field')[1].style.display = 'block';
+        document.getElementById('paragraph').innerHTML = "First to 30 Wins";
+        document.querySelector('.btn--field').style.display = 'none';
+        document.querySelector('.btn--addScore').style.display = 'none';
     }
+    lastRoll = dice;
+    if(gamePlays){
+        dice = Math.floor(Math.random() * 6) + 1;
+        if(dice === lastRoll && dice === 6 && lastRoll === 6){
+            scores[activePlayer] = 0;
+            document.getElementById('score--' + activePlayer).innerHTML = scores[activePlayer];
+            lastRoll = 0; dice = 0;
+            nextPlayer();
+        }else{
+            document.querySelector('.dice').style.display = 'block';
+            document.querySelector('.dice').src = 'dice-' + dice + '.png';
+            if(dice !== 1){
+                currentScore += dice;
+                lastScore = currentScore - dice;
+                document.getElementById('current--' + activePlayer).textContent = currentScore;
+            }else{
+                nextPlayer();        
+            }
+        }
     }
 })
 
 document.querySelector('.btn--hold').addEventListener('click', () => {
     if(gamePlays){
-    //Add current score to global score
     scores[activePlayer] += currentScore;
     document.getElementById('score--' + activePlayer).innerHTML = scores[activePlayer];
-    if(scores[activePlayer] >= 30){
+    lastRoll = 0; dice = 0;
+    if(scores[activePlayer] >= maxScore){
         document.getElementById('name--' + activePlayer).textContent = "Winner!";
         document.querySelector('.player--' + activePlayer).classList.add('player--winner');
         gamePlays = false;
     }
     nextPlayer();
     }
-
-
 })
 
-document.querySelector('.btn--new').addEventListener('click', init);
+document.querySelector('.btn--new').addEventListener('click', () => {
+    init();
+    document.querySelector('.btn--field').style.display = 'block';
+    document.querySelector('.btn--addScore').style.display = 'block';
+
+});
 
 nextPlayer = () => {
     if(gamePlays){
